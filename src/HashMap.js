78febc2edd5360ce3a50ab427cdb6bd1544356ff
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-05 18:54:47 +0700

    feat: add 'set' method

--- src/HashMap.js
+++ src/HashMap.js
@@ -8,10 +8,10 @@ export default class HashMap {
 	constructor() {
 		this.#capacity = 12;
 		this.#loadFactor = 0.75;
-		this.#buckets = new Array(this.#capacity).fill([]);
+		this.#buckets = new Array(this.#capacity).fill(null);
 	}
 
-	hash(key) {
+	#hash(key) {
 		const primeNumber = 31;
 		let hashCode = 0;
 
@@ -20,5 +20,31 @@ export default class HashMap {
 		}
 
 		return hashCode % this.#buckets.length;
+	}
+
+	set(key, value) {
+		const index = this.#hash(key);
+
+		if (index < 0 || index >= this.#buckets.length) {
+			throw new Error("Trying to access index out of bounds");
+		}
+
+		const newNode = new Node(key, value);
+		let currentNode = this.#buckets[index];
+
+		if (!currentNode) {
+			this.#buckets[index] = newNode;
+			return;
+		}
+
+		while (currentNode.nextNode) {
+			if (currentNode.key === key) {
+				currentNode.value = value;
+				return;
+			}
+			currentNode = currentNode.nextNode;
+		}
+
+		currentNode.nextNode = newNode;
 	}
 }
--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import HashMap from "./HashMap.js";
 
 const map = new HashMap();
 
-console.log(map.hash("Sara"));
-console.log(map.hash("raSa"));
+map.set("Sara", "red");
+map.set("Rasa", "yellow");
+map.set("Sara", "black");
+//console.log(map.set("Armin"));
+
+/*
+Move linked list method to new linked list class!
+*/
