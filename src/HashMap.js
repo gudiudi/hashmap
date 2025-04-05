4e92ef3555bd708e82b03fd3d8c39f0143553b90
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-05 17:11:29 +0700

    feat: add 'hash' method and package.json

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,3 +1,22 @@
 import Node from "./Node.js";
 
-export default class HashMap {}
+export default class HashMap {
+	#capacity;
+	#loadFactor;
+
+	constructor() {
+		this.#capacity = 12;
+		this.#loadFactor = 0.75;
+	}
+
+	hash(key) {
+		const primeNumber = 31;
+		let hashCode = 0;
+
+		for (const char of key) {
+			hashCode = primeNumber * hashCode + char.charCodeAt();
+		}
+
+		return hashCode % this.#capacity;
+	}
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import HashMap from "./HashMap.js";
+
+const map = new HashMap();
+
+console.log(map.hash("Sara"));
+console.log(map.hash("raSa"));
